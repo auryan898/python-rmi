df044366986c0daaf1fe1c90d805b9ac2a680bd0
Ryan Au <auryan898@gmail.com>
2022-10-12 19:39:46 -0400

    Updated setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Python-RMI',
+    version='0.1',
+    description='Python Remote Method Invocation',
+    author='Ryan Au',
+    author_email='auryan898@gmail.com',
+    url='https://github.com/auryan898/python-rmi',
+    packages=['pyrmi']
+)
