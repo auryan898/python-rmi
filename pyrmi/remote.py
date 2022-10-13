54ce8556c542afd770966b4a1edc877f8cf66709
Ryan Au <auryan898@gmail.com>
2022-10-13 01:58:08 -0400

    Add documentation and improved exception handling

--- pyrmi/remote.py
+++ pyrmi/remote.py
@@ -1,3 +1,4 @@
+
 try:
     from math import inf
 except:
@@ -15,6 +16,7 @@ import uuid
 import json
 import marshal
 import pickle
+
 
 TIMEOUT = socket.getdefaulttimeout()
 DEFAULT_PORT = 2110
@@ -39,7 +41,7 @@ def isrelatedclass(typ, cls):
 
 
 class IdentifyingException(Exception):
-    """An exception with an overriden string representation, telling its class name along with error message."""
+    """An exception with an overriden string representation, giving its class name along with error message."""
 
     def __repr__(self):
         return f'{self.__class__.__name__}: {super(IdentifyingException, self).__repr__()}'
@@ -61,6 +63,7 @@ class brickle:
         pass
 
     def dumps(obj):
+        """Only accepts objects that are of the type PasswordProtected, and converts them to a bytes-like format"""
         try:
             res = {}
             if isinstance(obj, PasswordProtected):
@@ -77,6 +80,11 @@ class brickle:
         return res
 
     def loads(data):
+        """Only accepts data of the bytes-like format used by this class. 
+        Outputs objects that are of the Command or Message type.
+
+        Returns None for any other type.
+        """
         try:
             data = brickle._parser.loads(data)
             if data['__class__'] == 'Command':
@@ -104,6 +112,7 @@ class PasswordProtected:
         self.password = password
 
     def verify_password(self, test):
+        """Verifies that the test input matches the stored internal password"""
         return test == self.password
 
 
@@ -112,14 +121,24 @@ class MessageReplyException(IdentifyingException):
 
 
 class Message(PasswordProtected):
+    """Objects of this class are used as a wrapper around text messages sent over a TCP socket.
+
+    Utilized mainly by the Connection, RemoteClient, and RemoteServer classes in this module.
+    """
+
     def __init__(self, text):
         super(Message, self).__init__()
         self.text = str(text)
         self.sender = None
 
     def reply(self, text):
-        # Intended for Connection objects, but can apply to anything
-        # that has a send method that can handle a Message obj
+        """Sends a reply to the original sender of this message. Accepts a string text message.
+
+        This action is not always available, and would raise a MessageReplyException if it isn't available.
+
+        The underlying sender is intended to be Connection objects, but can apply to anything
+        that has a send method that can handle a Message obj.
+        """
         if self.sender is not None and hasattr(self.sender, 'send') and callable(getattr(self.sender, 'send')):
             self.sender.send(Message(text))
         else:
@@ -130,7 +149,14 @@ class Message(PasswordProtected):
 
 
 class Command(PasswordProtected):
+    """An object that represents a function call along with its arguments.
+    It is intended for usage in communicating the data of remote function calls over sockets.
+
+    Utilized mainly by the Connection, RemoteClient, and RemoteServer classes in this module.
+    """
+
     def __init__(self, func_name, *args, **kwargs):
+        """Accepts the function name and arguments of the function call."""
         super(Command, self).__init__()
         self.func_name = func_name
         self.args = args
@@ -145,6 +171,7 @@ class Command(PasswordProtected):
 
 
 class Debuggable:
+    """An extra class utilized for displaying debug messages"""
     DEBUG_ALL = {}
     DEBUG_COUNTER = 0
 
@@ -162,15 +189,21 @@ class Debuggable:
             print(f'>>> ({i}) {text}\t', file=sys.stderr)
 
 
-class ConnectionError(IdentifyingException):
+class ConnectionListenerError(IdentifyingException):
+    """An error displayed when an error occurs on the Connection listener execution"""
     pass
 
 
 class ConnectionFatalError(IdentifyingException):
+    """An error displayed when a bad error occurs related to the operation of the Remote server"""
     pass
 
 
 class Connection:
+    """Objects that wrap TCP sockets and create a thread to listen for received data.
+    It also allows for listeners to be added, that process the data when it is received.
+    """
+
     def __init__(self, sock, password="password", debug=None):
         self.sock: socket.socket = sock
         self.listeners = {}
@@ -214,7 +247,7 @@ class Connection:
                             listener(*args, o, self)
                             # self._debug(f'completed listener "{key}"')
                         except Exception as err:
-                            c = ConnectionError(
+                            c = ConnectionListenerError(
                                 f"Error: Listener {key} - {err} {val}")
                             print(c, file=sys.stderr)
                 self.lock_listener.release()
@@ -230,6 +263,7 @@ class Connection:
         # self._debug(f'connection thread ended')
 
     def send(self, obj):
+        """Send an object over the Connection. Only accepts objects of the type PasswordProtected."""
         if isinstance(obj, PasswordProtected):
             self.lock_send.acquire()
             obj.password = self.password
@@ -259,6 +293,7 @@ class Connection:
         self.close()
 
     def close(self):
+        """Ends this connection's thread and closes the socket."""
         try:
             self.run_event.clear()
             self.sock.shutdown(socket.SHUT_RDWR)
@@ -269,15 +304,31 @@ class Connection:
         self._isclosed = True
 
     def isclosed(self):
+        """Checks if the socket is closed."""
         return self._isclosed
 
 
 class MethodCallerException(IdentifyingException):
+    """An error that occurs in the calling of methods on the host server."""
     pass
 
 
 class _MethodCaller:
+    """A class that wraps an object for remote method control.
+
+    This MethodCaller object will accept Command objects, and execute the 
+    Command's function call on the underlying wrapped object.
+    """
+
     def __init__(self, obj, custom=None, var_name=''):
+        """Create the wrapper around the object to be exposed for remote method control.
+        By default, excludes any methods starting with two underscores '__'
+
+        obj - the object to wrap
+        custom - Either None (default), or a list of string names of functions that would
+            also be included in the functions being exposed for remote method control.
+        var_name - Acts as a key, that can represent the Remote Object. Helps avoid name conflicts.
+        """
         if custom is None:
             custom = []
 
@@ -289,28 +340,53 @@ class _MethodCaller:
             self.cls) if func_name in custom or (callable(getattr(self.cls, func_name)) and not func_name.startswith("__"))}
 
     def supports_command(self, command: Command):
+        """Returns True if the function call of the Command is supported by this caller."""
         return command.func_name in self.methods
 
     def execute(self, command: Command):
+        """Executes the Command on the underlying wrapped object. 
+
+        If the command is not supported, the result is not set.
+        If an exception occurs during execution, the Command's 
+            result is set to the string representation of the Exception.
+        """
         if command.func_name in self.methods:
             try:
                 command.result = self.methods[command.func_name](self.obj,
                                                                  *command.args, **command.kwargs)
+                command._result_given = True
             except Exception as err:
                 command.result = str(MethodCallerException(err))
+                command._result_exception = True
         return command
 
 
-class MessageReceiver(object):  # Somewhat abstract class that needs self.conn to be set
+class MessageReceiver(object):
+    """Subclass this class to add a thread-safe message buffer structure.
+    Only provides methods for retrieving messages.
+
+    Similar to a mixin, that addes the messages and lock_messages attributes, 
+    and related functions for retrieving.
+    """
+
     def __init__(self):
         self.messages = deque()
         self.lock_messages = threading.Lock()
 
     def wait_messages(self, timeout=None, wait_interval=None):
+        """Busy waits for messages to arrive in the buffer.
+
+        timeout - the number of seconds to wait for in total
+        wait_interval - the sleep interval for a single iteration
+
+        returns True whenever the function returns.
+        """
         if timeout is None:
             timeout = inf
         if wait_interval is None:
             wait_interval = BUSY_WAITING
+
+        timeout = int(timeout / wait_interval)
 
         while timeout > 0 and not self.has_messages():
             timeout -= 1
@@ -318,9 +394,11 @@ class MessageReceiver(object):  # Somewhat abstract class that needs self.conn t
         return True
 
     def has_messages(self):
+        """Checks if the buffer contains any messages."""
         return self.num_messages() > 0
 
     def num_messages(self):
+        """Returns the number of messages in the buffer."""
         self.lock_messages.acquire()
         r = len(self.messages)
         self.lock_messages.release()
@@ -328,6 +406,8 @@ class MessageReceiver(object):  # Somewhat abstract class that needs self.conn t
 
     def get_messages(self, count=0):
         """Gets the specified number of messages from the message buffer.
+
+        Set to 0 (default value) to retrieve all the messages from the buffer.
         Thread-safe.
         """
         # Receiving messages in the listener thread for this RemoteBrick socket
@@ -346,13 +426,14 @@ class MessageReceiver(object):  # Somewhat abstract class that needs self.conn t
         return result
 
     def get_message(self, wait=False):
+        """Retrieves one message from the buffer."""
         if wait:
             self.wait_messages()
         m = self._get_message()
         return m
 
     def _get_message(self):
-        """Gets the one message from the message buffer, or None if none present.
+        """Gets one message from the message buffer, or None if none present.
         Thread-safe.
         """
         self.lock_messages.acquire()
@@ -365,11 +446,28 @@ class MessageReceiver(object):  # Somewhat abstract class that needs self.conn t
 
 
 class _RemoteCaller:
+    """A class that wraps a representative object to be the remote method controller.
+
+    Usual method calls on this RemoteCaller object will send a Command object 
+    through the associated RemoteClient object, instead of executing the usual method call.
+    It will then wait for a response from the RemoteClient object, and return the result of that instead.
+    """
     TESTING = False
 
     def create_caller(obj, remote_client, custom=None, var_name=''):
+        """Alters the given object (obj) such it represents a Remote Object.
+
+        Its functions will be modified to instead send Command objects through the RemoteClient (remote_client).
+
+        obj - the object to be altered to represent the Remote Object. Should be of the same type as the Remote Object.
+            This action relies on this obj having at least the same methods as the Remote Object.
+        remote_client - the RemoteClient object that commands will be sent through
+        custom - Either None (default), or a list of string names of functions that would
+            also be included in the functions being exposed for remote method control.
+        var_name - Acts as a key, that can represent the Remote Object. Helps avoid name conflicts.
+        """
         caller = _RemoteCaller(remote_client, var_name)
-        
+
         if custom is None:
             custom = []
 
@@ -386,7 +484,9 @@ class _RemoteCaller:
         self.var_name = var_name
 
     def _generate(self, func_name):
+        """Creates special methods that replace the existing methods in the remote object."""
         func_name = f'{self.var_name}.{func_name}'
+
         def func(*args, wait_for_data=60, **kwargs):
             res = self.remote_client._send_command(
                 func_name, *args, wait_for_data=wait_for_data, **kwargs)
@@ -406,9 +506,27 @@ class RemoteException(Exception):
 
 
 class RemoteClient(MessageReceiver):
+    """The client for remote method invocation.
+
+    Objects of this class can send and receive textual messages 
+    to the host, and create remote objects using the RemoteClient.create_caller method.
+    create_caller takes in an object (ideally of the same type as the remote object) and 
+    changes all of its methods such that they send function calls to the host and wait 
+    for responses from the host for return values.
+    """
+
     TESTING = False
 
     def __init__(self, address, password, port=None, sock=None):
+        """Creates the client for remote method invocation.
+
+        address - a string of either IP Address or Hostname of the Remote host
+        password - the password used by the remote host
+        port - None sets port to DEFAULT_PORT. Otherwise expects an integer value for port
+            to connect to.
+        sock - None creates a new socket based on the address and port. Otherwise, expects
+            an opened socket that is ready for sending and receiving data.
+        """
         super(RemoteClient, self).__init__()
 
         self.address = socket.gethostbyname(address)
@@ -430,15 +548,27 @@ class RemoteClient(MessageReceiver):
         self.conn.register_listener('main', RemoteClient._listener, (self,))
 
     def create_caller(self, obj, custom=None, var_name=''):
+        """Alters the given object (obj) such it represents a Remote Object.
+
+        Its functions will be modified to instead send Command objects through the RemoteClient (remote_client).
+
+        obj - the object to be altered to represent the Remote Object. Should be of the same type as the Remote Object.
+            This action relies on this obj having at least the same methods as the Remote Object.
+        custom - Either None (default), or a list of string names of functions that would
+            also be included in the functions being exposed for remote method control.
+        var_name - Acts as a key, that can represent the Remote Object. Helps avoid name conflicts.
+        """
         return _RemoteCaller.create_caller(obj, self, custom=custom, var_name=var_name)
 
     def send_message(self, text):
+        """Sends a string text message to the host"""
         self.conn.send(Message(text))
 
     def __del__(self):
         self.close()
 
     def close(self):
+        """Closes this connection to the host."""
         try:
             self.conn.close()
         except:
@@ -466,7 +596,7 @@ class RemoteClient(MessageReceiver):
         if wait_for_data:
             res = self._get_result(c.id, wait_for_data)
             if res._result_exception and not RemoteClient.TESTING:
-                raise RemoteException(res.result)
+                raise RemoteException(str(res.result))
         else:
             res = c.id
 
@@ -501,7 +631,22 @@ class RemoteClient(MessageReceiver):
 
 
 class RemoteServer(MessageReceiver):
+    """The client for remote method invocation.
+
+    Objects of this class can broadcast and receive textual messages 
+    to and from clients. Can accept objects to expose them 
+    for remote control by clients through the register_object method. 
+
+    Messages received from clients, will have the sender attribute set, 
+    such that through these Message objects one can reply to the client.
+    """
+
     def __init__(self, password, port=None):
+        """Simply accepts the password to authenticate received objects.
+
+        Optionally accepts a different port number. Expects type integer.
+        Defualts to DEFAULT_PORT when port=None.
+        """
         super(RemoteServer, self).__init__()
         self.password = (DEFAULT_PASSWORD if password is None else password)
         self.port = (DEFAULT_PORT if port is None else port)
@@ -560,6 +705,15 @@ class RemoteServer(MessageReceiver):
             self.lock_messages.release()
 
     def register_object(self, obj, custom=None, var_name=''):
+        """Accepts an object to be controlled by this remote method invocation host.
+
+        Does not modify the object given.
+
+        obj - the object to control
+        custom - Either None (default), or a list of string names of functions that would
+            also be included in the functions being exposed for remote method control.
+        var_name - Acts as a key, that can represent the Remote Object. Helps avoid name conflicts.
+        """
         caller = _MethodCaller(obj, custom=custom, var_name=var_name)
         for method in caller.methods:
             self._caller_methods[method] = caller
@@ -603,6 +757,7 @@ class RemoteServer(MessageReceiver):
         self.close()
 
     def broadcast_message(self, text):
+        """Send a singular message to all connected clients."""
         m = Message(text)
         self.lock_connections.acquire()
         for conn in self.connections:
@@ -613,6 +768,7 @@ class RemoteServer(MessageReceiver):
         self.lock_connections.release()
 
     def close_connections(self):
+        """Close all client connections."""
         self.lock_connections.acquire()
         c = self.connections
         self.connections = []
@@ -627,6 +783,7 @@ class RemoteServer(MessageReceiver):
         c.clear()
 
     def close(self):
+        """Close this server and all client connections."""
         self._isclosed = True
         self.run_event.clear()
         self.close_connections()
@@ -637,4 +794,5 @@ class RemoteServer(MessageReceiver):
             pass
 
     def isclosed(self):
+        """Returns True if this server has been closed by .close()"""
         return self._isclosed
